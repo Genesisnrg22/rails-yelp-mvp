1b42bd0298e5b5779960b3f7a530157947a5934a
Genesis Rodriguez <genesisnatasha2218@gmail.com>
2024-02-23 16:06:33 -0400

    modificado

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,4 @@
 class ReviewsController < ApplicationController
-  before_action :set_restaurant, only: %i[new create]
   def index
     @reviews = Review.all
   end
@@ -16,14 +15,13 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-    def review_params
-      params.require(:review).permit(:content, :rating)
-    end
 
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
